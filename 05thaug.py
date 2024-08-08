f57ed74ffe03f510b5ed337ba030e1e7ddc811bd
venky <karnamvenky123@gmail.com>
2024-08-07 22:34:30 -0700

    message

---
+++ 05thaug.py
@@ -0,0 +1,53 @@
+#random module
+import random
+import random 
+
+b = random . randint(1,10)
+print(b)
+
+import random 
+li = ['bat','ball','helmet','stumps']
+a = random.choice(li) 
+print(a)
+
+for i in range(10):
+    print(random.radiant(1,10))
+
+#math module
+
+import math 
+a = math .sqrt(16)
+print(a)
+
+g = math.factorial(5) 
+print(g)
+
+#ceil() above near intiger value
+j = math.ceil(4.2)
+print(j)
+
+#floor below near intiger value
+k = math.floor(4.7) 
+print(k)
+
+
+#datetime module 
+
+import datetime 
+
+from datetime import time
+t =time(15,30,45)
+print(t)
+
+date = datetime.datetime.now()
+print(date)
+
+today = datetime.date.today()
+print(today)
+
+#calender module 
+import calendar
+cal = calendar. month (2024,8)
+print(cal)
+
+
